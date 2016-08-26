456814170fccd3f4c0235a1780becd1ac6f0d882
Daniel <oopdaniel@gmail.com>
2016-08-26 09:05:17 +0800

    passed sass test

--- client/dev/c.component.ts
+++ client/dev/c.component.ts
@@ -1,7 +1,10 @@
 import { Component } from '@angular/core';
+import {ViewEncapsulation} from '@angular/core';
 
 @Component({
   selector: 'c',
-  template: '<h2>Hello World</h2>'
+  template: '<div class="test-style"><h2>Hello World</h2></div>',
+  styles: [require('./c.component.scss')],
+  encapsulation: ViewEncapsulation.None
 })
 export class CComponent { }
